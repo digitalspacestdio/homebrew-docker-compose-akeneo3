c57f303edba7e7f800b37c77103cf6863215200f
scherepanov <sergey@cherepanov.org.ua>
2022-01-03 04:45:22 +0400

    brew formula

--- Formula/docker-compose-magento.rb
+++ Formula/docker-compose-magento.rb
@@ -3,12 +3,15 @@ require 'formula'
 class DockerComposeMagento < Formula
   url "https://github.com/digitalspacestdio/docker-compose-magento.git", :using => :git
   version "0.1.0"
+  revision 2
 
   depends_on 'coreutils'
   depends_on 'mutagen-io/mutagen/mutagen'
 
   def install
-    bin.install "docker-compose-magento"
+    #bin.install "docker-compose-magento"
+    libexec.install Dir["*"]
+    bin.write_exec_script libexec/"docker-compose-magento"
   end
 
   def caveats
