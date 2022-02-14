fd6a095b0d20ffb70a37131afdb269b495aa552e
scherepanov <sergey@cherepanov.org.ua>
2022-02-14 19:30:40 +0400

    FASTCGI_PARAM_HTTPS

--- docker/nginx/40-tune-fastcgi_params.sh
+++ docker/nginx/40-tune-fastcgi_params.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
 sed -i 's/fastcgi_param  REMOTE_ADDR.*/fastcgi_param  REMOTE_ADDR        127.0.0.1;/g' /etc/nginx/fastcgi_params
+
+tee -a /etc/nginx/fastcgi_params <<FASTCGI_PARAM_HTTPS
+
+set $fcgi_https $https;
+if ($http_x_forwarded_proto = "https") {
+    set $fcgi_https on;
+}
+fastcgi_param HTTPS $fcgi_https;
+
+FASTCGI_PARAM_HTTPS
