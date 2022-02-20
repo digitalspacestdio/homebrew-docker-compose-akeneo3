3ca98cd13d386857ce05ddecae67c9be8028297b
scherepanov <sergey@cherepanov.org.ua>
2022-02-20 19:54:30 +0400

    db import

--- Formula/docker-compose-magento.rb
+++ Formula/docker-compose-magento.rb
@@ -3,7 +3,7 @@ require 'formula'
 class DockerComposeMagento < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-magento.git", :using => :git
   version "0.1.7"
-  revision 5
+  revision 6
 
   depends_on 'coreutils'
   depends_on 'rsync'
