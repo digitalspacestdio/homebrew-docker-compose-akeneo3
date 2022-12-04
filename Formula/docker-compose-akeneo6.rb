462a42bb4f343d5072e81c0ad74aee9633764a34
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2022-12-04 22:13:28 +0400

    mysql alias

--- Formula/docker-compose-akeneo6.rb
+++ Formula/docker-compose-akeneo6.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class DockerComposeAkeneo6 < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-akeneo6.git", :using => :git
-  version "0.1.0"
-  revision 7
+  version "0.1.1"
+  revision 2
 
   depends_on 'gpatch'
   depends_on 'coreutils'
